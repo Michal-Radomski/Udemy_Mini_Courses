ac7ebcdff64d4118937209c7b34db925d98254de
Michal Radomski <michalra@op.pl>
2023-04-09 00:27:03 +0200

    Course - commit2

--- 04 JS Math Game/01/index.js
+++ 04 JS Math Game/01/index.js
@@ -1,1 +1,15 @@
-// console.log("test");
+const gameArea = document.querySelector(".game");
+// console.log(gameArea);
+gameArea.textContent = "hello world";
+gameArea.style.backgroundColor = "red";
+//* Own property
+gameArea.val = "Hidden";
+// console.log(gameArea);
+gameArea.addEventListener("click", (e) => {
+  console.log(e);
+  if (e.target.style.backgroundColor == "red") {
+    e.target.style.backgroundColor = "green";
+  } else {
+    e.target.style.backgroundColor = "red";
+  }
+});
