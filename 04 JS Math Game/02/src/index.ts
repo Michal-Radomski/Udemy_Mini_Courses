b10143475928b34076829a61a3d069731165d7c3
Michal Radomski <michalra@op.pl>
2023-04-09 23:00:03 +0200

    Course - commit1

--- 04 JS Math Game/02/src/index.ts
+++ 04 JS Math Game/02/src/index.ts
@@ -1,1 +1,14 @@
-console.log("Hello World of Gulp and JS Math!");
+// console.log("Test");
+
+const gameArea = document.querySelector(".game");
+const btn = document.createElement("button");
+btn.classList.add("btn", "btn-primary");
+const output = document.createElement("div");
+const answer = document.createElement("input");
+output.textContent = "Click the button to start the game";
+btn.textContent = "Start Game";
+gameArea!.append(output);
+gameArea!.append(btn);
+
+const opts = ["*", "/", "+", "-"];
+const game = { maxValue: 10, questions: 10, oper: [2] };
