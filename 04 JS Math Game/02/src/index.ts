30cde638877dea56f81d4577c35493949ef7c700
Michal Radomski <michalra@op.pl>
2023-04-09 23:29:02 +0200

    Course - commit2

--- 04 JS Math Game/02/src/index.ts
+++ 04 JS Math Game/02/src/index.ts
@@ -11,4 +11,28 @@ gameArea!.append(output);
 gameArea!.append(btn);
 
 const opts = ["*", "/", "+", "-"];
-const game = { maxValue: 10, questions: 10, oper: [2] };
+const game = { maxValue: 10, questions: 10, oper: [2], curQue: 0 };
+
+btn.addEventListener("click", startGame);
+
+// for(let i=0;i<100;i++){
+//     let val = Math.floor(Math.random()*(game.maxValue+1));
+//     //console.log(val);
+// }
+
+function startGame() {
+  game.curQue = 0;
+  buildQuestion();
+}
+
+function buildQuestion() {
+  if (game.curQue < game.questions) {
+    game.curQue++;
+    output.innerHTML = "";
+    let vals = [];
+    vals[0] = Math.floor(Math.random() * (game.maxValue + 1));
+    vals[1] = Math.floor(Math.random() * (game.maxValue + 1));
+    vals[2] = eval(vals[0] + opts[2] + vals[1]);
+    output.innerHTML = `${vals[0]} ${opts[2]} ${vals[1]} = ${vals[2]} `;
+  }
+}
