c3319de90fe2d2b1330afaa4cae63d1bda26db1e
Michal Radomski <michalra@op.pl>
2023-04-10 13:36:43 +0200

    Course - commit5

--- 04 JS Math Game/02/src/index.ts
+++ 04 JS Math Game/02/src/index.ts
@@ -1,12 +1,15 @@
-// console.log("Test");
-
 const gameArea = document.querySelector(".game");
 const btn = document.createElement("button");
-btn.classList.add("btn", "btn-primary");
+btn.classList.add("btn", "btn-danger");
 const output = document.createElement("div");
 const answer = document.createElement("input");
 output.textContent = "Click the button to start the game";
 btn.textContent = "Start Game";
+answer.setAttribute("type", "number");
+answer.setAttribute("max", 999 as any);
+answer.setAttribute("min", 0 as any);
+output.classList.add("output");
+answer.classList.add("boxAnswer");
 gameArea!.append(output);
 gameArea!.append(btn);
 
@@ -32,29 +35,50 @@ function buildQuestion() {
     let vals = [];
     vals[0] = Math.floor(Math.random() * (game.maxValue + 1));
     vals[1] = Math.floor(Math.random() * (game.maxValue + 1));
-
     game.oVals.sort(() => {
       return 0.5 - Math.random();
     });
-
-    if (game.oVals[0] == 1) {
+    if (game.oVals[0] === 1) {
       let temp = vals[0] * vals[1];
       vals.unshift(temp);
-      console.log(temp);
+      console.log({ temp });
     } else {
       vals[2] = eval(vals[0] + opts[game.oVals[0]] + vals[1]);
     }
     vals[3] = opts[game.oVals[0]];
-    console.log(vals);
+    console.log({ vals });
     let hiddenVal;
-    if (game.hiddenVal != 3) {
+    if (game.hiddenVal !== 3) {
       hiddenVal = game.hiddenVal;
     } else {
       hiddenVal = Math.floor(Math.random() * 3);
     }
     //console.log(game.oVals);
-    console.log(hiddenVal);
-    vals[hiddenVal] = "__";
-    output.innerHTML = `${vals[0]} ${vals[3]} ${vals[1]} = ${vals[2]} `;
+    console.log({ hiddenVal });
+    answer.value = "";
+    for (let i = 0; i < 3; i++) {
+      if (hiddenVal === i) {
+        output.append(answer);
+      } else {
+        maker(vals[i], "box");
+      }
+
+      if (i === 0) {
+        maker(vals[3], "boxSign");
+      }
+      if (i === 1) {
+        maker("=", "boxSign");
+      }
+    }
+
+    //vals[hiddenVal] = '__';
+    //output.innerHTML = `${} ${vals[3]} ${vals[1]} = ${vals[2]} `;
   }
+}
+
+function maker(v: string, cla: string) {
+  const temp = document.createElement("div");
+  temp.classList.add(cla);
+  temp.textContent = v;
+  output.append(temp);
 }
