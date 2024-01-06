dc2111c79b8f9d89753a0dd8a22a05dfad4a6e39
Michal Radomski <michalra@op.pl>
2024-01-06 02:05:26 +0100

    Resposive Portfolio course - commit 14

--- 07 Portfolio/client/src/utilities/commonUtils.ts
+++ 07 Portfolio/client/src/utilities/commonUtils.ts
@@ -1,6 +1,6 @@
 import Home from "../PortfolioContainer/Home/Home";
 import AboutMe from "../PortfolioContainer/AboutMe/AboutMe";
-// import Resume from "../PortfolioContainer/Resume/Resume";
+import Resume from "../PortfolioContainer/Resume/Resume";
 import Testimonial from "../PortfolioContainer/Testimonial/Testimonial";
 // import ContactMe from "../PortfolioContainer/ContactMe/ContactMe";
 
@@ -14,10 +14,10 @@ export const TOTAL_SCREENS = [
     screen_name: "AboutMe",
     component: AboutMe,
   },
-  // {
-  //   screen_name: "Resume",
-  //   component: Resume,
-  // },
+  {
+    screen_name: "Resume",
+    component: Resume,
+  },
   {
     screen_name: "Testimonial",
     component: Testimonial,
