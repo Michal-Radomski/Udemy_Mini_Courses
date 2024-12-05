f818eeec780a775ac24b6be6fc1d437bd8afc277
Michal Radomski <michalra@op.pl>
2024-12-05 22:00:29 +0100

    Litte update

--- 05 JWT/02/bash.sh
+++ 05 JWT/02/bash.sh
@@ -1,4 +1,4 @@
-# Mamanually creating HS256 Signature
+# Manually creating HS256 Signature
 echo -n "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE2OTIwOTI5ODQsImlzcyI6ImxvY2FsaG9zdCIsImV4cCI6MTY5MjEwMTk4NCwidXNlcklkIjoiYWRtaW4iLCJpc0FkbWluIjoidHJ1ZSJ9" | openssl dgst -sha256 -hmac "123456" -binary | openssl base64 -e -A | sed s/\+/-/g | sed 's/\//_/g' | sed -E s/=+$//
 
 # output: eaqTCkhqoHuisYXYS8Egtjdh9slkDxqOURIxHc01fc4
