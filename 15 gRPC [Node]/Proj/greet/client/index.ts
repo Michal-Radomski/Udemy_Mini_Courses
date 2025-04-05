79f91d42bd6cc8cf6382e9df0653cfda4c6618e4
Michal Radomski <michalra@op.pl>
2025-04-05 21:28:25 +0200

    gRPC with Node course - commit 04

---
+++ 15 gRPC [Node]/Proj/greet/client/index.ts
@@ -0,0 +1,116 @@
+import fs from "fs";
+import grpc from "@grpc/grpc-js";
+
+const { GreetRequest } = require("../proto/greet_pb");
+const { GreetServiceClient } = require("../proto/greet_grpc_pb");
+const { DATA } = require("../../constants");
+
+// eslint-disable-next-line no-unused-vars
+function doGreet(client: any) {
+  console.log("doGreet was invoked");
+  const req = new GreetRequest().setFirstName("Clement");
+
+  client.greet(req, (err: Error, res: any) => {
+    if (err) {
+      return console.log(err);
+    }
+
+    console.log(`Greet: ${res.getResult()}`);
+  });
+}
+
+// eslint-disable-next-line no-unused-vars
+function doGreetManyTimes(client: any) {
+  console.log("doGreetManyTimes was invoked");
+  const req = new GreetRequest().setFirstName("Clement");
+  const call = client.greetManyTimes(req);
+
+  call.on(DATA, (res: any) => {
+    console.log(`GreetManyTimes: ${res.getResult()}`);
+  });
+}
+
+// eslint-disable-next-line no-unused-vars
+function doLongGreet(client: any) {
+  console.log("doLongGreet was invoked");
+  const names = ["Clement", "Marie", "Test"];
+  const call = client.longGreet((err: Error, res: any) => {
+    if (err) {
+      return console.error(err);
+    }
+
+    console.log(`LongGreet: ${res.getResult()}`);
+  });
+
+  names
+    .map((name) => {
+      return new GreetRequest().setFirstName(name);
+    })
+    .forEach((req) => call.write(req));
+
+  call.end();
+}
+
+// eslint-disable-next-line no-unused-vars
+function doGreetEveryone(client: any) {
+  console.log("doGreetEveryone was invoked");
+  const names = ["Clement", "Marie", "Test"];
+  const call = client.greetEveryone();
+
+  call.on(DATA, (res: any) => {
+    console.log(`GreetEveryone: ${res.getResult()}`);
+  });
+
+  names
+    .map((name) => {
+      return new GreetRequest().setFirstName(name);
+    })
+    .forEach((req) => call.write(req));
+
+  call.end();
+}
+
+// eslint-disable-next-line no-unused-vars
+function doGreetWithDeadline(client: any, ms: number) {
+  console.log("doGreetWithDeadline was invoked");
+  const req = new GreetRequest().setFirstName("Clement");
+
+  client.greetWithDeadline(
+    req,
+    {
+      deadline: new Date(Date.now() + ms),
+    },
+    (err: Error, res: any) => {
+      if (err) {
+        return console.log(err);
+      }
+
+      console.log(`GreetWithDeadline: ${res.getResult()}`);
+    }
+  );
+}
+
+function main(): void {
+  const tls = true;
+  let creds;
+
+  if (tls) {
+    const rootCert = fs.readFileSync("./ssl/ca.crt");
+
+    creds = grpc.ChannelCredentials.createSsl(rootCert);
+  } else {
+    creds = grpc.ChannelCredentials.createInsecure();
+  }
+
+  const client = new GreetServiceClient("localhost:50051", creds);
+
+  doGreet(client);
+  doGreetManyTimes(client);
+  doLongGreet(client);
+  doGreetEveryone(client);
+  doGreetWithDeadline(client, 5000);
+  doGreetWithDeadline(client, 1000);
+  client.close();
+}
+
+main();
---
+++ 15 gRPC [Node]/Proj/greet/server/index.ts
@@ -0,0 +1,55 @@
+import fs from "fs";
+import grpc from "@grpc/grpc-js";
+
+const service = require("../proto/greet_grpc_pb");
+const serviceImpl = require("./service_impl");
+
+const addr = "0.0.0.0:50051";
+
+function cleanup(server: grpc.Server): void {
+  console.log("Cleanup");
+
+  if (server) {
+    server.forceShutdown();
+  }
+}
+
+function main(): void {
+  const server: grpc.Server = new grpc.Server({});
+
+  process.on("SIGINT", (): void => {
+    console.log("Caught interrupt signal");
+    cleanup(server);
+  });
+
+  const tls = true;
+  let creds;
+
+  if (tls) {
+    const rootCert = fs.readFileSync("./ssl/ca.crt");
+    const certChain = fs.readFileSync("./ssl/server.crt");
+    const privateKey = fs.readFileSync("./ssl/server.pem");
+
+    creds = grpc.ServerCredentials.createSsl(rootCert, [
+      {
+        cert_chain: certChain,
+        private_key: privateKey,
+      },
+    ]);
+  } else {
+    creds = grpc.ServerCredentials.createInsecure();
+  }
+
+  server.addService(service.GreetServiceService, serviceImpl);
+  server.bindAsync(addr, creds, (err, _) => {
+    if (err) {
+      return cleanup(server);
+    }
+
+    server.start();
+  });
+
+  console.log("Listening on: " + addr);
+}
+
+main();
