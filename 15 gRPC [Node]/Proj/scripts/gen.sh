43bf96cec986af748061d27e69e60a404ad82f4a
Michal Radomski <michalra@op.pl>
2025-04-05 20:53:29 +0200

    gRPC with Node course - commit 03

--- 15 gRPC [Node]/Proj/scripts/gen.sh
+++ 15 gRPC [Node]/Proj/scripts/gen.sh
@@ -4,20 +4,27 @@
 #   a .proto named after the project (eg: blog, blog.proto)
 #   and optionally some other .proto files
 
+# echo -n test
+# ./node_modules/.bin/grpc_tools_node_protoc --js_out=. --grpc_out=. greet/proto/greet.proto
+
 argc=$#
 argv=("$@")
 
-for ((j = 0; j < argc; ++j)); do
-  # Generate gRPC and Protobuf code for ${PROJECT}/${PROJECT}.proto
-  # (eg: greet/greet.proto)
-  ./node_modules/.bin/grpc_tools_node_protoc -I ${argv[j]}/proto/ \
-    --js_out=import_style=commonjs:${argv[j]}/proto/ \
-    --grpc_out=grpc_js:${argv[j]}/proto/ \
-    ${argv[j]}/proto/${argv[j]}.proto
+# echo $argc
+# echo $argv
 
-  # Generate only Protobuf code for all the other .proto files (if any)
-  # (eg: calculator/sum.proto)
-  ./node_modules/.bin/grpc_tools_node_protoc -I ${argv[j]}/proto/ \
-    --js_out=import_style=commonjs:${argv[j]}/proto/ \
-    $(find ${argv[j]}/proto/ -type f -name "*.proto" -not -path "${argv[j]}/proto/${argv[j]}.proto")
-done
+#* Not working?!!!
+# for ((j = 0; j < argc; ++j)); do
+#   # Generate gRPC and Protobuf code for ${PROJECT}/${PROJECT}.proto
+#   # (eg: greet/greet.proto)
+#   ./node_modules/.bin/grpc_tools_node_protoc -I ${argv[j]}/proto/ \
+#     --js_out=import_style=commonjs:${argv[j]}/proto/ \
+#     --grpc_out=grpc_js:${argv[j]}/proto/ \
+#     ${argv[j]}/proto/${argv[j]}.proto
+
+#   # Generate only Protobuf code for all the other .proto files (if any)
+#   # (eg: calculator/sum.proto)
+#   ./node_modules/.bin/grpc_tools_node_protoc -I ${argv[j]}/proto/ \
+#     --js_out=import_style=commonjs:${argv[j]}/proto/ \
+#     $(find ${argv[j]}/proto/ -type f -name "*.proto" -not -path "${argv[j]}/proto/${argv[j]}.proto")
+# done
